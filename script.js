c29d0414717601669626b3ef4a4f1ef78062aa12
Mahdiyeh <101658092+mahdiyeh-asgharpour@users.noreply.github.com>
2022-10-21 16:18:39 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,60 @@
-while (true) {
-    var player2 = (Math.random() * 3);
-    var p1 = 0;
-    var p2 = 0;
-    function rock() {
-        if ((number(player2)) <= 1) {
-            player2 = 'rock';
+let p1 = 0;
+let p2 = 0;
 
-        } else if ((number(player2)) <= 2) {
-            player2 = 'paper';
-            p2++;
-
-        } else if ((number(player2)) <= 3) {
-            player2 = 'scissors';
-            p1++;
-
-        }
-        alert("You :" + String(p1) + "Robot : " + String(p2));
-
-
+function getRobotPick() {
+    const randomNumber = Math.random() * 3;
+    if (randomNumber <= 1) {
+        return "rock";
+    } else if (randomNumber <= 2) {
+        return "paper";
+    } else {
+        return "scissors";
     }
-    function paper() {
-        if ((number(player2)) <= 1) {
-            player2 = 'rock';
-            p1++;
+}
 
-        } else if ((number(player2)) <= 2) {
-            player2 = 'paper';
-
-        } else if ((number(player2)) <= 3) {
-            player2 = 'scissors';
-            p2++;
-
-        }
-        alert("You :" + String(p1) + "Robot : " + String(p2));
-
-
+function checkRoundWinner(yourPick, robotPick) {
+    if (yourPick == robotPick) {
+        return;
+    } else if (yourPick == "rock" && robotPick == "scissors") {
+        p1++;
+        return;
+    } else if (yourPick == "paper" && robotPick == "rock") {
+        p1++;
+        return;
+    } else if (yourPick == "scissors" && robotPick == "paper") {
+        p1++;
+        return;
+    } else {
+        p2++;
+        return;
     }
-    function scissors() {
-        if ((number(player2)) <= 1) {
-            player2 = 'rock';
-            p2++;
+}
 
-        } else if ((number(player2)) <= 2) {
-            player2 = 'paper';
-            p1++;
+function alertPicksAndScore(yourPick, robotPick) {
+    let str = `You picked ${yourPick} - Robot picked ${robotPick}
+You: ${p1} Robot: ${p2}
+  `;
+    document.getElementById('alert').innerText = str;
+}
 
-        } else if ((number(player2)) <= 3) {
-            player2 = 'scissors';
-
-        }
-        alert("You :" + String(p1) + "Robot : " + String(p2));
-
-
-    }
+function endGame() {
     if (p1 == 3 || p2 == 3) {
         if (p1 == 3 && p2 == 3) {
-            alert('Tie');
-            break;
+            document.getElementById('endgame').innerText = "Tie";
         } else if (p1 == 3 && p2 != 3) {
-            alert('Win');
-            break;
+            document.getElementById('endgame').innerText = "Win";
         } else if (p2 == 3 && p1 != 3) {
-
-
-            alert('Lose');
-            break;
+            document.getElementById('endgame').innerText = "Lose";
         }
+        p1 = 0;
+        p2 = 0;
 
     }
+}
 
+function gameLoop(userPick) {
+    const robotPick = getRobotPick();
+    checkRoundWinner(userPick, robotPick);
+    alertPicksAndScore(userPick, robotPick);
+    endGame();
 }
